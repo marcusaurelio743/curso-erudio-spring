08652fc08c70ea442c70e7a1da711999b45dc33a
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2026-09-24 19:55:42 -0300

    commit projeto

--- br.com.springProject/src/main/java/br/com/springProject/controllers/MathController.java
+++ br.com.springProject/src/main/java/br/com/springProject/controllers/MathController.java
@@ -1,7 +1,6 @@
 package br.com.springProject.controllers;
 
-import javax.websocket.server.PathParam;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +10,28 @@ public class MathController {
 	
 	
 	@RequestMapping("/sum/{numberOne}/{numberTwo}")
-	public Double sum( 
-			@PathParam("numberOne") String numberOne,
-			@PathParam("numberTwo") String numberTwo
-	) {
-		return 1D;
+	public Double sum  ( 
+			@PathVariable("numberOne") String numberOne,
+			@PathVariable("numberTwo") String numberTwo
+	) throws Exception {
+		if(! isnumeric(numberOne) || ! isnumeric(numberTwo)) 
+			throw new Exception();
+		
+		return converterToDouble(numberOne) +converterToDouble( numberTwo);
+		}
+
+	private Double converterToDouble(String number) {
+		if(!isnumeric(number)) throw new IllegalArgumentException();
+		return Double.valueOf(number);
 	}
+
+	private boolean isnumeric(String strnumber) {
+		if(strnumber == null || strnumber.isEmpty()) return false;
+		
+		String number = strnumber.replace(",", ".");
+		
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+"); 
+	}
+		
+	
 }
