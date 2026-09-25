b43de4ce685d956df115f10ea9211370c16cc4dc
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2026-09-24 21:06:35 -0300

    commit do projeto

--- br.com.springProject/src/main/java/br/com/springProject/controllers/MathController.java
+++ br.com.springProject/src/main/java/br/com/springProject/controllers/MathController.java
@@ -21,6 +21,62 @@ public class MathController {
 		
 		return converterToDouble(numberOne) +converterToDouble( numberTwo);
 		}
+	
+	@RequestMapping("/sub/{numberOne}/{numberTwo}")
+	public Double sub  ( 
+			@PathVariable("numberOne") String numberOne,
+			@PathVariable("numberTwo") String numberTwo
+	) throws Exception {
+		if(! isnumeric(numberOne) || ! isnumeric(numberTwo)) 
+			throw new UnSuportOperationException("please set is numeric value");
+		
+		return converterToDouble(numberOne) - converterToDouble( numberTwo);
+		}
+	
+	@RequestMapping("/multi/{numberOne}/{numberTwo}")
+	public Double multi  ( 
+			@PathVariable("numberOne") String numberOne,
+			@PathVariable("numberTwo") String numberTwo
+	) throws Exception {
+		if(! isnumeric(numberOne) || ! isnumeric(numberTwo)) 
+			throw new UnSuportOperationException("please set is numeric value");
+		
+		return converterToDouble(numberOne) * converterToDouble( numberTwo);
+		}
+	
+	@RequestMapping("/div/{numberOne}/{numberTwo}")
+	public Double div  ( 
+			@PathVariable("numberOne") String numberOne,
+			@PathVariable("numberTwo") String numberTwo
+	) throws Exception {
+		if(! isnumeric(numberOne) || ! isnumeric(numberTwo)) 
+			throw new UnSuportOperationException("please set is numeric value");
+		
+		return converterToDouble(numberOne) / converterToDouble( numberTwo);
+		}
+	
+	@RequestMapping("/media/{numberOne}/{numberTwo}")
+	public Double media  ( 
+			@PathVariable("numberOne") String numberOne,
+			@PathVariable("numberTwo") String numberTwo
+	) throws Exception {
+		if(! isnumeric(numberOne) || ! isnumeric(numberTwo)) 
+			throw new UnSuportOperationException("please set is numeric value");
+		
+		return (converterToDouble(numberOne) + converterToDouble( numberTwo)) /2;
+		}
+	
+	@RequestMapping("/raiz/{number}")
+	public Double raiz  ( 
+			@PathVariable("number") String number
+			
+	) throws Exception {
+		if(! isnumeric(number) ) 
+			throw new UnSuportOperationException("please set is numeric value");
+		
+		return (Math.sqrt(converterToDouble(number)));
+		}
+	
 
 	private Double converterToDouble(String number) {
 		if(!isnumeric(number)) throw new UnSuportOperationException("please set is numeric value");
